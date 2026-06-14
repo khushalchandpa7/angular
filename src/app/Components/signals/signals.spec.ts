257afe54084a168d9cf2cd2890ff8467943c4961
Khushal Chandpa <khushalchandpa7@gmail.com>
2026-06-14 16:57:21 +0530

    [KC] ng test failed error solved

--- src/app/Components/signals/signals.spec.ts
+++ src/app/Components/signals/signals.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Signal } from './signals';
+import { Signals } from './signals';
 
 describe('Signal', () => {
-  let component: Signal;
-  let fixture: ComponentFixture<Signal>;
+  let component: Signals;
+  let fixture: ComponentFixture<Signals>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Signal],
+      imports: [Signals],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Signal);
+    fixture = TestBed.createComponent(Signals);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
